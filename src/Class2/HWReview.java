1d44567264ee300f8100d8e610ff7d7b3f1484f0
Mariya Bazeleva <mariyabazeleva@Mariyas-MacBook-Pro.local>
2020-05-03 17:18:28 -0400

    First homework

---
+++ src/Class2/HWReview.java
@@ -0,0 +1,37 @@
+//Homework review and correction
+
+package Class2;
+
+
+public class HWReview {
+
+    public static void main(String[] args) {
+
+        //Correction for Task2
+        //use lastIndex variable to fetch value from words-array
+
+        String myStatement = "I am a good programmer";
+        String[] words = myStatement.split(" ");
+        int lastIndex = words.length-1;
+        System.out.println("The last index of the sentence is: " + lastIndex);
+
+
+
+        //Correction for Task3
+        /* Deepak: "If this is the solution for:
+            Calculate the length of your name
+            without using length() method of String class
+            then try to use split method to get each letter as an element of array.
+            then calculate length of array (using array.length)
+         */
+
+        String myFirstName = "Mariya";
+        String[] letters = myFirstName.split("");
+        int lengthOfName = letters.length;
+        System.out.println("The length of my name is: " + lengthOfName);
+
+        //I thought we couldn't use .length method at all.
+
+
+    }
+}
